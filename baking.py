ff960ab5741117c76b8f408949ca7bfd7b4e5fe1
Dre <developerdrequanwalker@gmail.com>
2023-09-13 13:10:16 -0400

    Test Recipe

--- baking.py
+++ baking.py
@@ -11,14 +11,14 @@ def print_ingredients():  # Ways to work with strings/printing
     # Those are the right ingredients, aren't they? Let's print them out a few different ways to check
  
     # 1.1 Using one print statement, print out the ingredients as 4 separate strings
-  print(ingredient_1, ingredient_2, ingredient_3, ingredient_4)
+    print(ingredient_1, ingredient_2, ingredient_3, ingredient_4)
     # 1.2 Using one print statement, use string concatenation to print the ingredients
     # as a single string (make sure to add a space between each ingredient!)
-  print(ingredient_1 + ' ' + ingredient_2 + ' ' + ingredient_3 + ' ' + ingredient_4)
+    print(ingredient_1 + ' ' + ingredient_2 + ' ' + ingredient_3 + ' ' + ingredient_4)
 
     # 1.3 Using one print statement, use an f-string to print the ingredients
     # as a single string (make sure to add a space between each ingredient!)
-  print(f"{ingredient_1} {ingredient_2} {ingredient_3} {ingredient_4}")
+    print(f"{ingredient_1} {ingredient_2} {ingredient_3} {ingredient_4}")
 
 
 def confirm_ingredients():  # String methods
@@ -77,6 +77,7 @@ def favorite_bake():  # User Input/Type conversion
     print(f'Ooooh, {baked_good}s are delicious!')
     print(f'We recommend you eat {baked_good}s at least {frequency * 2} times a month!')
 
+# Call the functions
 print_ingredients()
 confirm_ingredients()
 favorite_bake()
