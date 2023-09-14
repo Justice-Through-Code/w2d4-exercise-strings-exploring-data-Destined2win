19e2e3610e9c47ec6e949c940c75a0bedf586c05
Dre <developerdrequanwalker@gmail.com>
2023-09-14 17:52:41 -0400

    Run Code

--- baking.py
+++ baking.py
@@ -45,7 +45,7 @@ def confirm_ingredients():  # String methods
     count_milk = ingredients.count('milk')
     
     # 2.4 Let's print out our `ingredients` variable again just to be sure.
-    print(count_milk)
+    print(ingredients)
 
     # 'milk' is still in there! To save our changes to the string, we'll need to update the variable.
     # 2.5 Update our `ingredients` variable using the replace method, the same way we printed it out in step 2.2
